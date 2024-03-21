f4748336f0911cff7995604497fbab5fcd8ca390
deeptanshunayak <deeptanshunayak@gmail.com>
2024-03-21 16:06:07 +0530

    update

--- src/Form.jsx
+++ src/Form.jsx
@@ -4,9 +4,11 @@ import FormfacadeEmbed from "@formfacade/embed-react";
 
 export default function Form() {
   return (
+    <div className='overflow1'>
     <div className='text-center backer'>
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdTRrHgTGstpE73QZstr3-SgF2cSMM0bnHWuHYRLHv8Q95VLw/viewform?embedded=true" width="880" height="1838" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
      
+    </div>
     </div>
   )
 }
