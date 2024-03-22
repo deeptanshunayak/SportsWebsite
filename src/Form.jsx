f6bfd8422c4da47a9267dc739b2888e072d6481c
deeptanshunayak <deeptanshunayak@gmail.com>
2024-03-22 21:58:17 +0530

    update

--- src/Form.jsx
+++ src/Form.jsx
@@ -5,14 +5,13 @@ import FormfacadeEmbed from "@formfacade/embed-react";
 export default function Form() {
   return (
     <div className='overflow1'>
-    <div className='text-align2 backer displayer'>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdTRrHgTGstpE73QZstr3-SgF2cSMM0bnHWuHYRLHv8Q95VLw/viewform?embedded=true" width="880" height="1838" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-     
-    </div>
-    <div className='text-align2 backer displayer1'>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdTRrHgTGstpE73QZstr3-SgF2cSMM0bnHWuHYRLHv8Q95VLw/viewform?embedded=true" width="410" height="1838" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-     
-    </div>
+      <FormfacadeEmbed
+
+formFacadeURL="https://formfacade.com/include/115577192390460274657/form/1FAIpQLSdTRrHgTGstpE73QZstr3-SgF2cSMM0bnHWuHYRLHv8Q95VLw/classic.js/?div=ff-compose"
+
+onSubmitForm={() => console.log('Form submitted')}
+
+/>
     </div>
   )
 }
